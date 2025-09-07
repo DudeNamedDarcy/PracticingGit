de93cdc38e2cd3bf7e2a8fcb76c78f28533a389d
Darcy Loane-Billings <122037471+DudeNamedDarcy@users.noreply.github.com>
2025-09-07 13:59:13 -0400

    Added JS Files to this Repo!

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log(isNaN(1));
+function neatFunc(p1, p2)
+{
+    console.log("Subtracting p2 by p1: " + (p2 - p1))
+}
+neatFunc(42, 67);
+let hehe = 9;
+console.log("the let 'hehe' equals: " + hehe);
